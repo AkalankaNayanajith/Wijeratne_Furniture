f991bb8f326a46b024dd222b695311d5c67e9f0e
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2022-11-16 08:09:28 +0530

    testing input text btn

--- src/Components/CustomizedOrders.jsx
+++ src/Components/CustomizedOrders.jsx
@@ -4,6 +4,12 @@ export default function CustomizedOrders() {
   return (
    <>
     <div className='text-4xl text-center bold w-full mt-[4rem]'> Custom Orders </div>
-        </>
+
+    <div className='h-full w-full text-center mt-9'>
+      <input type="text" name="" id="" placeholder="Email@email.com" className="text-xl p-4 h-16 w-[50%] rounded-lg outline-none border-black border-2 focus:border-2 focus:border-blue-500 focus:ring-2 focus:ring-blue-500"/>
+    </div>
+
+
+    </>
   )
 }
