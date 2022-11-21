2b1f6570fb1d183fc066e20e7dfce8a4bf46fe84
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2022-11-22 00:03:50 +0530

    product card changed a bit

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -5,16 +5,16 @@ function ProductCard({ name, price, review, prodImage }) {
   return (
     <div className="m-8">
       <div className="max-w-xs bg-white rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl">
-        <a href="singleproduct">  
+        <a href="oneproductview">  
           <img
             className="w-full h-72 p-8 rounded-t-lg"
-            src="../Images/prod5.png"
-            //   src={prodImage}
+            // src="../Images/prod5.png"
+              src={prodImage}
             alt="prod"
           />
         </a>
         <div className="px-5 pb-5">
-          <a href="singleproduct"> 
+          <a href="oneproductview"> 
             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
               {name}
             </h5>
@@ -25,7 +25,7 @@ function ProductCard({ name, price, review, prodImage }) {
 
           <div className="flex justify-between items-center mt-3">
             <a
-              href="singleproduct"           //product page path link
+              href="oneproductview"           //product page path link
               className="text-white w-full bg-black hover:bg-[#1c1d1d] focus:ring-4 focus:outline-none focus:ring-[#2a2c2c]  font-medium rounded-lg text-sm px-[5rem] py-2.5 text-center dark:bg-[#1c1d1d]  dark:hover:bg-red-700 dark:focus:ring-red-800"
             >
             SHOP NOW
