2ecabb8147e417f8a090030a17b94e2cee87f205
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2022-11-30 09:24:37 +0530

    size increase when hover,..max,min height removed

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -4,7 +4,7 @@ import Ratings from "./Ratings";
 function ProductCard({  review,  newprodname, prodid ,category, description, quantity, dicount, price, color, material, country, imgpath, onClickHandle }) {
   return (
     <div className="m-8">
-      <div className="max-w-xs min-h-[36rem] max-h-[36rem] bg-white rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl" onClick={onClickHandle}>
+      <div className="max-w-xs  bg-white rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl ease-in-out hover:scale-105 duration-300" onClick={onClickHandle}>
         <a href="oneproductview">  
           <img
             className="w-full h-72 p-8 rounded-t-lg"
