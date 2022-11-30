d98852a92349fc7983bafee91d0285534a80c5b9
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2022-11-30 14:20:47 +0530

    changes

--- src/Components/TrackMyOrder.jsx
+++ src/Components/TrackMyOrder.jsx
@@ -1,38 +1,9 @@
-import Ratings from "./Ratings";
-import React, { useState, useEffect } from "react";
-import ProductCard from "./ProductCard";
+import React from 'react'
 
-export default function TrackMyOrder({review,newprodname,prodid, category,description, quantity,dicount,price,color,material,country,imgpath,}) {
-//here it is not track my order, but here I retrieve data from db and show them in product cards
-//edit later
-// same code here copied for products.jsx file
-
-  const [productcardss, setProductcardss] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/product/getAll")
-      .then((response) => response.json())
-      .then((result) => {
-        setProductcardss(result);
-      });
-  }, []);
-
-
-
+const TrackMyOrder = () => {
   return (
-    <>
-      <div className=" container flex">
-        {productcardss.map((product) => (
-          
-          <ProductCard
-            newprodname={product.newprodname}
-            price={product.price}
-            imgpath={product.imgpath}
-            key={product.prodid}
-            review={review}/>
-
-        ))}
-      </div>
-    </>
-  );
+    <div>TrackMyOrder</div>
+  )
 }
+
+export default TrackMyOrder
