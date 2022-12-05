1ebb40c04598f5621e76dfe83d7153bc9d5d18f4
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2022-12-05 11:06:10 +0530

    sort list drop down updated

--- src/Components/SortListDropdown.jsx
+++ src/Components/SortListDropdown.jsx
@@ -2,35 +2,18 @@ import React from "react";
 
 const SortListDropdown = () => {
   return (
-    <>
-      
-      <button id="dropdownDefault" dataDropdownToggle="dropdown" className="text-black bg-[#DFDFDF] hover:bg-blue-800 hover:text-white focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-none text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-[#DFDFDF] dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button 
-      
-      <svg className="ml-2 w-4 h-4" ariaHidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7">
-          </path>
-     </svg>
-          
-          </button>
-
-<div id="dropdown" className=" z-20 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 absolute">
-    <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" ariaLabelledby="dropdownDefault">
-      <li>
-        <a href="wwww" className="block py-2 px-4 text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-      </li>
-      <li>
-        <a href="www" className="block py-2 px-4 text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-      </li>
-      <li>
-        <a href="ww" className="block py-2 px-4 text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-      </li>
-      <li>
-        <a href="ww" className="block py-2 px-4 text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-      </li>
-    </ul>
-
-</div>
-    </>
+ 
+    <select id="Sort" name="Sort" className=" bg-[#DFDFDF] text-lg  h-14 w-full p-4 rounded-none outline-none border-[#DBD9D9] border-2 focus:border-2 focus:border-blue-500 focus:ring-2 focus:ring-blue-500  ">
+                    <option value="Best Selling">Best Selling</option>
+                    <option value="Featured">Featured</option>
+                    <option value="Alphabetically, A-Z">Alphabetically, A-Z </option>
+                    <option value="Alphabetically, Z-A">Alphabetically, Z-A </option>
+                    <option value="Date,New to Old">Date,New to Old</option>
+                    <option value="Date,Old to New">Date,Old to New</option>
+                    <option value="Price, High to Low">Price, High to Low</option>
+                    <option value="Price, Low to High">Price, Low to High</option>
+        </select>
+  
   );
 };
 
