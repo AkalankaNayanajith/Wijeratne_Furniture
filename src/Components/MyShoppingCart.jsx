f39901c520b9f1df267f08c86d70c43e85c25a0b
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2023-02-15 10:02:12 +0530

    remove hard coded part

--- src/Components/MyShoppingCart.jsx
+++ src/Components/MyShoppingCart.jsx
@@ -29,59 +29,10 @@ const MyShoppingCart = () => {
             </th>
           </tr>
 
-
    
           <CartProdComponent/>
 
 
-  {/* from here */}
-          <tr className="border-b-2 mb-6 w-full border-[#DBD9D9]">
-            <td>
-              <div className="h-28 flex mt-4 mb-4 items-center">
-                <div className="h-24 w-24 ml-[13.5rem] rounded-lg border-2 border-[#A6A6A6]">
-                  <img
-                    className="h-[90%] w-[90%] m-auto mt-1"
-                    src="../Images/prod4.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="pl-[3rem] ">
-                  <h3 className="text-[#34383b] max-w-sm min-w-sm font-sans text-xl ">
-                    luxury Double Bed
-                  </h3>
-                 
-                  <p className="font-light mt-[0.1rem] text-[#585d61] font-sans text-sm italic">
-              
-                    Teak Wood ,Ultra comfort Soft mattress
-                  </p>
-                  <button>
-          
-                    <p className="text-[#CB6202] mt-[0.1rem] underline underline-offset-2 ">
-                      Remove
-                    </p>
-                  </button>
-                </div>
-                
-              </div>
-             
-            </td>
-            <td>
-              {" "}
-              <p className="text-[#34383b] text-right font-sans text-xl ">
-                Rs 67,999{" "}
-              </p>
-            </td>
-            <td className="items-end text-right">
-              <input
-                className="h-12 text-xl  border-[#A6A6A6] w-16 text-center border-2 rounded-md mr-20 "
-                type="number" min="1"
-              />
-            </td>
-            <td> <p className="text-[#34383b] font-sans text-right text-xl pr-52">
-                Rs 67,999{" "}
-              </p></td>
-          </tr>
-{/* to here one component */}
 
         </table>
 
