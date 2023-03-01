ca09ce9aafb08da0836ca59f6b40a92631473db7
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2023-03-02 03:58:31 +0530

    paypal gateway checked

--- src/Components/TrackMyOrder.jsx
+++ src/Components/TrackMyOrder.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
 
 const TrackMyOrder = () => {
   return (
-    <div>TrackMyOrder</div>
+    <div className='mt-36 ml-[35rem] '>
+      <PayPalScriptProvider options={{"client-id" : "AXbCCGoZ2_0grW1ZOhd5nsFnSUQQq-oCQktBVeuXLAIUUI1PtjUjwZABeWddxfvW3E0QdjGyl2_GWqkq"}}>
+        <PayPalButtons createOrder={(data, actions) => {
+                    return actions.order
+                        .create({
+                            purchase_units: [
+                                {
+                                    amount: {
+                                        currency_code: "Rs",
+                                        value: "99.99",
+                                    },
+                                },
+                            ],
+                        })
+                      }} 
+                      >
+        </PayPalButtons>
+      </PayPalScriptProvider>
+    </div>
   )
 }
 
