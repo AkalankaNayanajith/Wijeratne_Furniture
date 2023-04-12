576bc1e58988dc843df6848558a758ab75300d8d
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2023-04-12 18:13:25 +0530

    toggle like number -/+ button for number input

---
+++ src/Components/Buttons/NumberInput.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function NumberInputButton() {
+  const [value, setValue] = useState(1);
+
+  const handleDecrease = () => {
+    if (value > 1){ 
+    setValue(value - 1);}
+    else {setValue (value) };
+  };
+
+  const handleIncrease = () => {
+    setValue(value + 1);
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <button
+        className="rounded-l-md h-8 px-3 py-auto border bg-gray-500  text-white hover:bg-slate-700 focus:outline-none "
+        onClick={handleDecrease}
+      >
+        -
+      </button>
+      <input
+        className="w-12 h-8 px-2 py-2 border-t border-b text-center bg-white text-black focus:outline-none  "
+        type="number"
+        value={value}
+        min="1"    
+        onChange={(e) => setValue(Number(e.target.value))}
+      />
+      <button
+        className="rounded-r-md h-8 px-3 py-auto border  bg-gray-500 text-white hover:bg-slate-700 focus:outline-none "
+        onClick={handleIncrease}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default NumberInputButton;
