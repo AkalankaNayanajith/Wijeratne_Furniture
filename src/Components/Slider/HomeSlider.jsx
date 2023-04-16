f6bea97f48d09d63775f637ba9af9e808a07e4da
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2023-04-16 15:45:57 +0530

    Home slider still not working

--- src/Components/Slider/HomeSlider.jsx
+++ src/Components/Slider/HomeSlider.jsx
@@ -1,9 +1,11 @@
+import { faAngleLeft, faAngleRight, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 const slides = [
   { id: 1, image: "../Images/Hero_1.jpg", alt: "Slide 1" },
-  { id: 2, image: "../Images/Hero_1.jpg", alt: "Slide 2" },
-  { id: 3, image: "../Images/Hero_1.jpg", alt: "Slide 3" },
+  { id: 2, image: "../Images/Hero_2.jpg", alt: "Slide 2" },
+  { id: 3, image: "../Images/Hero_4.jpg", alt: "Slide 3" },
 ];
 
 function HomeSlider() {
@@ -20,18 +22,16 @@ function HomeSlider() {
   return (
     <div className="relative">
       <div className="overflow-hidden w-full">
-        <div className="relative flex transition-all duration-100 ease-in-out">
+        <div className="relative flex transition-all duration-100 ease-in-out ">
           {slides.map((slide, index) => (
-            <div
-              key={slide.id}
-              className={`w-1200 h-500 flex-shrink-0 transition-transform duration-500 ease-in-out ${
-                index === currentSlide ? "translate-x-0" : "translate-x-full"
-              }`}
-            >
+            <div key={slide.id}
+              className={`w-1200 h-500 flex-shrink-0 transition-transform duration-500 ease-in-out relative border-2 border-red-500 ${
+                index === currentSlide ? "translate-x-0" : "translate-x-full" }`}>
               <img src={slide.image} alt={slide.alt} className="object-cover w-full h-full" />
-              <button className="bg-gray-800 hover:bg-gray-700 text-white text-lg font-medium absolute inset-y-1/2 transform -translate-y-1/2 left-1/2 px-6 py-4 rounded-full">
+              <button className=" text-white text-lg font-medium absolute inset-y-1/2 transform -translate-y-1/2 -translate-x-1/2 left-1/2 px-10 py-6 rounded-full border-2 border-white items-center flex hover:scale-105 hover:border-4">
                 Shop Now
               </button>
+              
             </div>
           ))}
         </div>
@@ -50,13 +50,13 @@ function HomeSlider() {
         </div>
       </div>
       <div className="absolute top-1/2 left-0 transform -translate-y-1/2">
-        <button className="bg-gray-800 hover:bg-gray-700 text-white text-lg font-medium px-6 py-2 rounded-full" onClick={prevSlide}>
-          Previous
+      <button className=" text-white text-4xl font-medium px-6 py-2 rounded-full" onClick={prevSlide}>
+        <FontAwesomeIcon icon={faAngleLeft} className='fill-white' > </FontAwesomeIcon>
         </button>
       </div>
       <div className="absolute top-1/2 right-0 transform -translate-y-1/2">
-        <button className="bg-gray-800 hover:bg-gray-700 text-white text-lg font-medium px-6 py-2 rounded-full" onClick={nextSlide}>
-          Next
+        <button className=" text-white text-4xl font-medium px-6 py-2 rounded-full" onClick={nextSlide}>
+        <FontAwesomeIcon icon={faAngleRight} className='fill-white' > </FontAwesomeIcon>
         </button>
       </div>
     </div>
