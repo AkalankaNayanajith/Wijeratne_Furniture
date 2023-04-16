e9a22cb41924247a778503fc7320f77385cfdf93
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2023-04-16 13:07:06 +0530

    expanding section tested

--- src/Components/ShowBox.jsx
+++ src/Components/ShowBox.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import ImageSelectionForm from './ImageSelectionForm';
 
 function ShowBox() {
   const [showBox, setShowBox] = useState(false);
@@ -13,12 +14,13 @@ function ShowBox() {
     <div className="p-4">
       <button
         className="flex items-center justify-center gap-2 py-2 px-4 bg-gray-200 rounded-lg text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500"
-        onClick={handleButtonClick}
-      >
-        Show <FontAwesomeIcon icon={faCaretDown} className={`transition-transform duration-300 transform ${showBox ? 'rotate-180' : ''}`} />
+        onClick={handleButtonClick}  >
+        Table Edges <FontAwesomeIcon icon={faCaretDown} className={`transition-transform duration-300 transform ${showBox ? 'rotate-180' : ''}`} />
       </button>
       {showBox && (
-        <div className="border border-black w-96 h-20 mt-4"></div>
+        <div className=" rounded-lg  mt-4">
+          <ImageSelectionForm/>
+        </div>
       )}
     </div>
   );
